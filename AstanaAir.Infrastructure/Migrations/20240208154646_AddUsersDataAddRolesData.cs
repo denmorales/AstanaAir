e92739554a19f4e6bdc20b63b36c83fffb18eabb
Denis Matveev <matveev@itexp.pro>
2024-02-08 18:47:46 +0300

    feat: добавлены миграции добавления базовых пользователей и ролей

---
+++ AstanaAir.Infrastructure/Migrations/20240208154646_AddUsersDataAddRolesData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AstanaAir.Infrastructure.Migrations
+{
+    public partial class AddUsersDataAddRolesData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Code" },
+                values: new object[,]
+                {
+                    { 1, "Moderator" },
+                    { 2, "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Password", "RoleId", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "pass1", 2, "user1" },
+                    { 2, "pass2", 1, "moderator1" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
